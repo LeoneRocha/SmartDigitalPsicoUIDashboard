856d68c445e445cc54cabcda83d8098a38f4e028
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-10-19 16:15:58 -0300

    Ajustando para criar testes

---
+++ src/app/app.componentmodelo.spec.ts
@@ -0,0 +1,34 @@
+// componentemodelo.component.spec.ts
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentemodeloComponent } from './app.componentmodelo';
+
+describe('ComponentemodeloComponent', () => {
+  let component: ComponentemodeloComponent;
+  let fixture: ComponentFixture<ComponentemodeloComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ComponentemodeloComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ComponentemodeloComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have a default title', () => {
+    expect(component.title).toBe('default title');
+  });
+
+  it('should change the title when setTitle is called', () => {
+    component.setTitle('new title');
+    expect(component.title).toBe('new title');
+  });
+});
