714cf8a3b3ba3a84c34f8675e7c09d580422da4b
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-10-19 15:29:17 -0300

    Ajuste cadastro medico

--- src/app/models/principalsmodel/MedicalModel.ts
+++ src/app/models/principalsmodel/MedicalModel.ts
@@ -1,13 +1,17 @@
 import { BaseEntityModel } from "../contracts/BaseEntityModel";
 import { ETypeAccreditation } from "../general/eTypeAccreditation";
 import { OfficeModel } from "../simplemodel/OfficeModel";
-import { SpecialtyModel } from "../simplemodel/SpecialtyModel"; 
+import { SpecialtyModel } from "../simplemodel/SpecialtyModel";
 
-export interface MedicalModel extends BaseEntityModel { 
+export interface MedicalModel extends BaseEntityModel {
     office?: OfficeModel
     officeId: number;
     specialtiesIds: Array<number>;
-    specialties?: SpecialtyModel[] 
-    accreditation: string
-    typeAccreditation: number 
+    specialties?: SpecialtyModel[];
+    accreditation: string;
+    typeAccreditation: number;
+    startWorkingTime: string; // Usando string para simplificar
+    endWorkingTime: string;
+    workingDays: Array<number>; 
+    patientIntervalTimeMinutes: number;
 }
