f1a1777061db93829a1d06104e1234dbdcf40321
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-12-31 20:58:51 -0300

    Parcial 1

--- src/app/custompages/calendar/calendar.component.ts
+++ src/app/custompages/calendar/calendar.component.ts
@@ -9,16 +9,19 @@ import { EventUtility } from 'app/helpers/eventUtility';
 //https://fullcalendar.io/demos
 
 //or  https://github.com/mattlewis92/angular-calendar/tree/v0.30.1
+
 declare var $: any;
 
 @Component({
 	moduleId: module.id,
 	selector: 'calendar-cmp',
-	templateUrl: 'calendar.component.html',
+	templateUrl: 'calendar.component.html'
 })
+
 export class CalendarComponent implements OnInit {
 	@ViewChild('fullcalendar') fullcalendar: FullCalendarComponent;
 	events;
+	eventsData;
 	calendarOptions: CalendarOptions;
 	parentId: number;
 	userLoged: any;
@@ -29,7 +32,7 @@ export class CalendarComponent implements OnInit {
 		, @Inject(AuthService) private authService: AuthService) {
 	}
 
-	ngOnInit(): void {
+	loadDataFromApi(): void {
 		const today = new Date();
 		const y = today.getFullYear();
 		const m = today.getMonth();
@@ -42,18 +45,15 @@ export class CalendarComponent implements OnInit {
 			filterDaysAndTimesWithAppointments: false,
 			filterByDate: today,
 			userIdLogged: this.userLoged?.id
-		};
-
-		this.events = EventUtility.generateEvents();
-		console.log('----------------------ngOnInit-------------------------');
-		console.log(this.events);
-		
+		}; 
 		this.userLoged = this.authService.getLocalStorageUser();
 
 		this.calendarEventService.getCalendarEvents(criteria).subscribe(events => {
-			//this.events = events;
-			this.initializeCalendar();
-		});
+			this.eventsData = events; 
+		}); 
+		
+		console.log('----------------------loadDataFromApi- getCalendarEvents -------------------------');
+		console.log(this.eventsData);
 	}
 	getParentId(): number {
 		const paramsUrl = this.route.snapshot.paramMap;
@@ -64,15 +64,84 @@ export class CalendarComponent implements OnInit {
 		return medicalId;
 	}
 
-	initializeCalendar() { 
+	loadExampleData(): void {
+
+		const today = new Date();
+		const y = today.getFullYear();
+		const m = today.getMonth();
+		const d = today.getDate();
+		this.events = [
+			{
+				title: 'All Day Event',
+				start: new Date(y, m, 1),
+				className: 'event-default'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: new Date(y, m, d + 3, 6, 0),
+				allDay: false,
+				className: 'event-rose'
+			},
+			{
+				title: 'Meeting',
+				start: new Date(y, m, d - 1, 10, 30),
+				allDay: false,
+				className: 'event-green'
+			},
+			{
+				title: 'Lunch',
+				start: new Date(y, m, d + 7, 12, 0),
+				end: new Date(y, m, d + 7, 14, 0),
+				allDay: false,
+				className: 'event-red'
+			},
+			{
+				title: 'Md-pro Launch',
+				start: new Date(y, m, d - 2, 12, 0),
+				allDay: true,
+				className: 'event-azure'
+			},
+			{
+				title: 'Birthday Party',
+				start: new Date(y, m, d + 1, 19, 0),
+				end: new Date(y, m, d + 1, 22, 30),
+				allDay: false,
+				className: 'event-azure'
+			},
+			{
+				title: 'Click for Creative Tim',
+				start: new Date(y, m, 21),
+				end: new Date(y, m, 22),
+				url: 'https://www.creative-tim.com/',
+				className: 'event-orange'
+			},
+			{
+				title: 'Click for Google',
+				start: new Date(y, m, 21),
+				end: new Date(y, m, 22),
+				url: 'https://www.creative-tim.com/',
+				className: 'event-orange'
+			}
+		];
+	}
+	ngOnInit(): void {
+
+		this.events = EventUtility.generateEvents();
+		console.log('----------------------ngOnInit-------------------------');
+		console.log(this.events);
+
+		this.loadDataFromApi();
+		//this.loadExampleData();
+
 		this.calendarOptions = {
 			headerToolbar: {
 				right: 'prev,next today',
 				center: 'dayGridMonth,timeGridWeek,timeGridDay',
-				left: 'title',
+				left: 'title'
 			},
 			initialView: 'dayGridMonth',
-			initialEvents: this.events, // alternativamente, use a configuração `events` para buscar de um feed
+			initialEvents: this.events, // alternatively, use the `events` setting to fetch from a feed
 			weekends: true,
 			editable: true,
 			selectable: true,
@@ -82,41 +151,41 @@ export class CalendarComponent implements OnInit {
 			displayEventTime: true,
 			views: {
 				month: {
-					titleFormat: { month: 'long', year: 'numeric' },
+					titleFormat: { month: "long", year: "numeric" }
 				},
 				agendaWeek: {
-					titleFormat: { month: 'long', year: 'numeric', day: 'numeric' },
+					titleFormat: { month: "long", year: "numeric", day: "numeric" }
 				},
 				agendaDay: {
-					titleFormat: { month: 'short', year: 'numeric', day: 'numeric' },
-				},
+					titleFormat: { month: "short", year: "numeric", day: "numeric" }
+				}
 			},
 			dateClick: this.addEvent.bind(this),
+
 		};
 	}
-
-	handleDateClick() { }
-
-	addEvent(event) {
-		swal
-			.fire({
-				title: 'Create an Event',
-				customClass: {
-					confirmButton: 'btn btn-fill btn-primary',
-					cancelButton: 'btn btn-default',
-					input: 'swl-input',
-				},
-				showCancelButton: true,
-				confirmButtonText: 'Save',
-				input: 'text',
-				inputPlaceholder: 'Event Title',
-			})
-			.then(result => {
-				this.fullcalendar.getApi().addEvent({
-					title: result.value,
-					start: event.date,
-					className: 'event-default',
-				});
-			});
+	handleDateClick() {
 	}
-}
+	addEvent(event) {
+		swal.fire({
+			title: 'Create an Event',
+
+			customClass: {
+				confirmButton: "btn btn-fill btn-primary",
+				cancelButton: "btn btn-default",
+				input: "swl-input"
+			},
+			showCancelButton: true,
+			confirmButtonText: "Save",
+			input: "text",
+			inputPlaceholder: "Event Title"
+
+		}).then((result) => {
+			this.fullcalendar.getApi().addEvent({
+				title: result.value,
+				start: event.date,
+				className: 'event-default'
+			});
+		})
+	}
+}	
--- src/app/services/general/calendar/calendar-event.service.ts
+++ src/app/services/general/calendar/calendar-event.service.ts
@@ -4,21 +4,22 @@ import { CalendarCriteriaDto } from 'app/models/medicalcalendar/CalendarCriteria
 import { CalendarDto } from 'app/models/medicalcalendar/CalendarDto';
 import { ServiceResponse } from 'app/models/ServiceResponse';
 import { MedicalCalendarService } from '../principals/medicalCalendar.service';
-import { DateHelper } from 'app/helpers/date-helper';
+import { DateHelper } from 'app/helpers/date-helper'; 
+import { ICalendarEvent } from 'app/models/general/ICalendarEvent';
+
 @Injectable({
   providedIn: 'root',
 })
 export class CalendarEventService {
   constructor(private medicalCalendarService: MedicalCalendarService) { }
-  
-  getCalendarEvents(criteria: CalendarCriteriaDto): Observable<any[]> {
+
+  getCalendarEvents(criteria: CalendarCriteriaDto): Observable<ICalendarEvent[]> {
     return this.medicalCalendarService.getMonthlyCalendar(criteria).pipe(
       map((response: ServiceResponse<CalendarDto>) => {
-
         const daysResult = DateHelper.sortTimeSlots(response.data.days);
         response.data.days = DateHelper.fillAddDayOfWeek(daysResult);
         const resultCalendarData = response.data;
-        const eventsResult = resultCalendarData.days.flatMap(day =>
+        const eventsResult: ICalendarEvent[] = resultCalendarData.days.flatMap(day =>
           day.timeSlots.map(slot => ({
             title: slot.isAvailable ? 'Available' : 'Not Available',
             start: DateHelper.convertToLocalTime(slot.startTime),
@@ -26,8 +27,8 @@ export class CalendarEventService {
             className: slot.isAvailable ? 'event-green' : 'event-gray'
           }))
         );
-        console.log('-------------------- getCalendarEvents --------------------');
-        console.log(eventsResult);
+        //console.log('-------------------- getCalendarEvents --------------------');
+        //console.log(eventsResult);
         return eventsResult;
       }),
       catchError(error => {
