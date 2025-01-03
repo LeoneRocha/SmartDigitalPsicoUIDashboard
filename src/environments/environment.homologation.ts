8539dc9b7f577ea2022fdb8b2f63f92e52cf4489
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2025-01-03 15:29:19 -0300

    Atualizando versao

--- src/environments/environment.homologation.ts
+++ src/environments/environment.homologation.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   EnableCaptureResponseTolog: false,
   APIUrl: 'https://smartdigitalpsicoapi-staging.azurewebsites.net/api', 
-  UIVersion: '2024.05.31'
+  UIVersion: '2025.01.02.01'
 };
