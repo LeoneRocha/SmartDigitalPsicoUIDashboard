05fb1da38d268bd8defedd15199ff6c651a27b10
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2025-01-07 13:27:51 -0300

    vers

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   EnableCaptureResponseTolog: false,
   APIUrl: 'https://smartdigitalpsicoapi.azurewebsites.net/api',
-  UIVersion: '2025.01.02.01'
+  UIVersion: '2025.01.06.01'
 };
